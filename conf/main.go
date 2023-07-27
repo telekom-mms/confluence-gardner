3bbe981589df458d97e9b0f36d076888bb60d08e
Sebastian Gumprich <sebastian.gumprich@t-systems.com>
2023-07-27 20:12:11 +0200

    add version flag

--- conf/main.go
+++ conf/main.go
@@ -9,6 +9,8 @@ import (
 	"github.com/spf13/viper"
 )
 
+var version string
+
 func ReadConf() {
 	viper.SetConfigName("application") // name of config file (without extension)
 	viper.AddConfigPath(".")           // optionally look for config in the working directory
@@ -18,14 +20,23 @@ func ReadConf() {
 }
 
 func ParseCliOpts() {
+
+	var versionFlag bool
+	pflag.BoolVar(&versionFlag, "version", false, "Print the version of the program")
+
 	pflag.StringP("confluence_url", "u", "https://confluence.example.com/rest/api", "The URL to the Confluence REST-API with http(s)")
 	pflag.StringP("confluence_token", "t", "", "The token to authenticate against the Confluence REST-API")
 	pflag.StringP("confluence_page_id", "i", "", "The ID for which to crawl child pages")
+
 
 	pflag.Parse()
 	err := viper.BindPFlags(pflag.CommandLine)
 	if err != nil {
 		log.Fatal(err)
+	}
+	if versionFlag {
+		fmt.Println(version)
+		os.Exit(0)
 	}
 
 	if viper.GetString("confluence_url") == "" {
